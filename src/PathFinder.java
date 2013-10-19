7b2e0490b40c862e9d59729c6633e2313ff261b7
Chris <christopher.h.fong@gmail.com>
2013-10-19 19:03:55 +1100

    Added interface and stub files for Pathfinding algorithms

---
+++ src/PathFinder.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Chris FONG
+ * @since : 19/10/13
+ *        Made with Love
+ */
+public interface PathFinder {
+
+    public ArrayList<Node> find(char from, char to);
+
+}
---
+++ src/ShortestDelay.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Chris FONG
+ * @since : 19/10/13
+ *        Made with Love
+ */
+public class ShortestDelay implements PathFinder {
+
+    @Override
+    public ArrayList<Node> find(char from, char to) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
